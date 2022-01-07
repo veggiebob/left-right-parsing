e4cea1ad496dda341de12b013bbc4338dce2aeb9
veggiebob <veggiebobowman@gmail.com>
2022-01-07 12:02:54 -0700

    initial

---
+++ src/lang_obj.rs
@@ -0,0 +1,58 @@
+use core::convert::From;
+use core::option::Option;
+use core::option::Option::None;
+use crate::parse::Parser;
+
+#[derive(Eq, PartialEq, Debug, Hash, Clone)]
+pub struct LOString {
+    pub content: String
+}
+
+#[derive(Eq, PartialEq, Debug, Hash, Clone)]
+pub struct LONat {
+    pub content: u64
+}
+
+#[derive(Eq, PartialEq, Debug, Hash, Clone)]
+pub enum Expr {
+    Nat(LONat),
+    Str(LOString),
+    Infix(Box<Expr>, String, Box<Expr>),
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct ParseError {
+    pub location: Option<usize>,
+    pub range: Option<(usize, usize)>,
+    pub message: String
+}
+
+impl From<String> for ParseError {
+    fn from(s: String) -> Self {
+        ParseError {
+            location: None,
+            range: None,
+            message: s
+        }
+    }
+}
+
+impl From<&str> for ParseError {
+    fn from(s: &str) -> Self {
+        String::from(s).into()
+    }
+}
+
+impl From<u64> for LONat {
+    fn from(n: u64) -> Self {
+        LONat {
+            content: n
+        }
+    }
+}
+
+impl From<LONat> for Expr {
+    fn from(n: LONat) -> Self {
+        Expr::Nat(n)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate maplit;
+use std::ops::Add;
+
+pub mod parse;
+pub mod lang_obj;
+pub mod funcs;
+pub mod test;
+
+fn main() {
+    println!("Hello, world!");
+}
